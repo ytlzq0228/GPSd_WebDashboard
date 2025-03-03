17a5a53656e862fac3037697ec73870624991afc
liziqi <123>
2025-03-03 17:30:28 +0800

    0303.01

---
+++ add_service_gpsd_webdashboard.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+mount -o remount,rw / ; sudo mount -o remount,rw /boot
+
+# 写入服务文件内容
+echo "Creating systemd service file..."
+cat <<EOF | sudo tee /etc/systemd/system/gpsd_webdashboard.service
+[Unit]
+Description=gpsd_webdashboard
+After=network.target gpsd.service
+
+[Service]
+ExecStart=/etc/GPSd_WebDashboard/gpsd_webdashboard.sh
+ExecStop=/usr/bin/pkill -f gpsdwebdash.py
+Restart=always
+RestartSec=5
+User=root
+WorkingDirectory=/etc/GPSd_WebDashboard/
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable gpsd_webdashboard.service
+systemctl start gpsd_webdashboard.service
+systemctl status gpsd_webdashboard.service
+sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
--- update_version.sh
+++ update_version.sh
@@ -5,8 +5,11 @@ mount -o remount,rw / ; sudo mount -o remount,rw /boot
 
 git reset --hard
 git pull origin main
+echo "Code pulled on $(date)"
+cp -r * /etc/GPSd_WebDashboard
+echo "Copy to /etc finished"
 
-python3 /home/pi-star/GPSd_WebDashboard/webdash.py
+systemctl restart aprs_reporter.service
 
 sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
 
