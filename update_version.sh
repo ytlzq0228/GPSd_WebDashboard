49b20b199e854c2a9ae99fad5d787bf854887878
liziqi <123>
2025-03-03 16:27:43 +0800

    0303.01

--- update_version.sh
+++ update_version.sh
@@ -6,7 +6,7 @@ mount -o remount,rw / ; sudo mount -o remount,rw /boot
 git reset --hard
 git pull origin main
 
-python3 ~/GPSd_WebDashboard/webdash.py
+python3 /home/pi-star/GPSd_WebDashboard/webdash.py
 
 sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
 
