ce03944f9ff411762def4c06aa66742f4bdf3d7d
liziqi <123>
2025-03-04 23:34:41 +0800

    0304.01

--- gpsdwebdash.py
+++ gpsdwebdash.py
@@ -68,7 +68,7 @@ def update_gps_data():
 				# 更新SNR数据
 				if data_json.get('class') == 'SKY' and 'satellites' in data_json:
 					for i in data_json['satellites']:
-						if i['used']:
+						if i['used'] and get_constellation(i['PRN']) not in gps_data_cache['SNR']['satellites']:
 							gps_data_cache['SNR']['satellites'].append({'PRN': get_constellation(i['PRN']), 'ss': i['ss']})
 				
 				# 更新TPV数据，保留了你的细节处理
