0e8b8bc8e62d23baa1662f64970f203255dd15a2
liziqi <123>
2025-03-05 09:57:47 +0800

    0305.01

--- gpsdwebdash.py
+++ gpsdwebdash.py
@@ -50,8 +50,7 @@ def get_constellation(prn):
 gps_data_cache = {
 	'SNR': {'satellites':[]},
 	'TPV': {},
-	'Path': {},
-	'Log': {}
+	'Path': {}
 }
 
 def update_gps_data():
@@ -97,8 +96,8 @@ def update_gps_data():
 							gps_data_cache['Path'][i]=data_json[i]
 					#阶梯化speed，不连续变化，避免speed频繁变化影响地图缩放
 					step=5
-					gps_data_cache['Path']['speed']=5
-					#gps_data_cache['Path']['speed']=max((round(gps_data_cache['Path']['speed'] / step) * step),1)
+					#gps_data_cache['Path']['speed']=5 #test only
+					gps_data_cache['Path']['speed']=max((round(gps_data_cache['Path']['speed'] / step) * step),1)
 
 		time.sleep(0.5)  # Reduce CPU usage
 
